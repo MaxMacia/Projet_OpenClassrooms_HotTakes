0fa6fd8fb9f8d3b4b0693a1fa6774c1cb6baf3d0
Maxence Macia <maxence.macia@gmail.com>
2022-12-03 23:44:01 +0100

    mise en place des routes et des controleurs pour user

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const User = require('./models/User');
 const app = express();
+
+const userRoutes = require('./routes/user');
 
 mongoose.connect(
     "mongodb+srv://MaxMacia:211089Mn@cluster0.ib5vpwf.mongodb.net/?retryWrites=true&w=majority",
@@ -24,44 +25,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/auth/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        return user.save()
-        .then(() => {
-            const message = "Utilisateur enregistré";
-            res.status(201).json(message);
-        })
-    })
-    .catch(error => res.status(500).json({ error }));
-});
-
-app.post('/api/auth/login', (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .then(user => {
-        if (user == null) {
-            const message = "Paire identifiant/mot de passe invalide";
-            res.status(401).json({ message });
-        } else {
-            return bcrypt.compare(req.body.password, user.password)
-            .then(valid => {
-                if (!valid) {
-                    const message = "Paire identifiant/mot de passe invalide";
-                    res.status(401).json({ message });
-                } else {
-                    res.status(200).json({
-                        userId: user._id,
-                        token: 'TOKEN'                        
-                    });
-                }
-            })
-        }
-    })
-    .catch(error => res.status(500).json({ error }));
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        return user.save()
+        .then(() => {
+            const message = "Utilisateur enregistré";
+            res.status(201).json(message);
+        })
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (user == null) {
+            const message = "Paire identifiant/mot de passe invalide";
+            res.status(401).json({ message });
+        } else {
+            return bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    const message = "Paire identifiant/mot de passe invalide";
+                    res.status(401).json({ message });
+                } else {
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'                        
+                    });
+                }
+            })
+        }
+    })
+    .catch(error => res.status(500).json({ error }));
+};
