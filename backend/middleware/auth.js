e3d68c5d64a01d76c1d41d7aa87813a95b176a47
Maxence Macia <maxence.macia@gmail.com>
2022-12-04 11:58:25 +0100

    installation jwt, mise en place d'un token codé avec l'userId, mise en place du middleware auth

---
+++ backend/middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOW_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+    } catch (error) { res.status(201).json({ error }) };
+    next();
+};
